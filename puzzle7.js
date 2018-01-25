218b2964f233c47a98b34c24c3d7ccef01312c06
natkasman <34670412+natkasman@users.noreply.github.com>
2018-01-25 15:42:32 -0800

    Create puzzle7.js

---
+++ puzzle7.js
@@ -0,0 +1,34 @@
+var firstColorSeen=getColor
+
+while (getColor !=“white”) {
+down
+}
+
+var secondColorSeen=getColor
+
+while (getColor !=“pink”) {
+right
+right
+}
+
+while (getColor !=“white”) {
+up
+}
+
+while (getColor !=“pink”) {
+right
+right
+}
+
+while (getColor !=“white”) {
+down
+}
+
+while (getColor !=“pink”) {
+right
+right
+}
+
+while (getColor !=“white”) {
+up
+}
