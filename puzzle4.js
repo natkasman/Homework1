8173db09ccd859954f779122e431426bfbd5edcd
natkasman <34670412+natkasman@users.noreply.github.com>
2018-01-25 15:41:27 -0800

    Create puzzle4.js

---
+++ puzzle4.js
@@ -0,0 +1,25 @@
+down
+down
+down
+down
+right
+var firstColorSeen=getColor
+
+if (getColor==“green”) {
+down
+setColor (firstColorSeen)
+right
+right 
+}
+
+up
+right
+var secondColorSeen=getColor
+
+if (getColor==“yellow”) {
+
+down
+setColor (secondColorSeen)
+right
+right
+}
