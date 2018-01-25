1b68595186efce909a4c847cbb7b3af2e8005611
natkasman <34670412+natkasman@users.noreply.github.com>
2018-01-25 15:42:52 -0800

    Create puzzle8.js

---
+++ puzzle8.js
@@ -0,0 +1,31 @@
+function GoDown {
+var steps = 0
+while (steps < 8) {
+  down
+  steps = steps + 1
+}
+
+right
+right
+
+function GoUp {
+var steps = 0
+while (steps < 8) {
+  down
+  steps = steps + 1
+}
+}
+
+function GoDown
+
+right
+right
+
+function GoUp
+
+right
+right
+
+function GoDown
+
+right
