2bcf24518ea6187391c0172dcd79f095f816afb2
natkasman <34670412+natkasman@users.noreply.github.com>
2018-01-25 15:42:03 -0800

    Create puzzle5.js

---
+++ puzzle5.js
@@ -0,0 +1,19 @@
+right
+right
+var firstColorSeen=getColor
+
+if (getColor ==“blue”) {
+down
+right
+right
+}
+
+var secondColorSeen=getColor
+
+if (getColor==“pink”) {
+up
+right
+right
+}
+
+right
