ab2d79d80fb76fa6f01f6f69fa9730e459478ca3
natkasman <34670412+natkasman@users.noreply.github.com>
2018-01-25 23:58:44 -0800

    Create puzzle6.js

---
+++ puzzle6.js
@@ -0,0 +1,24 @@
+right 
+
+var firstColorSeen=getColor
+if (firstColorSeen=pink) {
+up
+up 
+up
+}
+
+if (firstColorSeen=pink) {
+left
+up
+}
+
+if (firstColorSeen=blue) {
+down
+down 
+down
+}
+
+if (firstColorSeen=blue) {
+right
+up
+}
