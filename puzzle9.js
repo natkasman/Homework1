f9aa0f24fa9d77f0ee40c81f42c21b53acc10e92
natkasman <34670412+natkasman@users.noreply.github.com>
2018-01-29 11:05:31 -0800

    Update puzzle9.js

--- puzzle9.js
+++ puzzle9.js
@@ -1,19 +1,37 @@
-var steps = 0
-while (steps < 9) {
-  down
-  steps = steps + 1
+function GoDown() {
+  var steps = 0
+  while (steps < 9) {
+    down();
+    steps = steps + 1
+	}
+}
 
-var steps = 0
-while (steps < 9) {
-  right
-  steps = steps + 1
+function GoRight() {
+  var steps = 0
+  while (steps < 9) {
+    right();
+    steps = steps + 1
+	}
+}
 
-var steps = 0
-while (steps < 9) {
-  up
-  steps = steps + 1
 
-var steps = 0
-while (steps < 9) {
-  left
-  steps = steps + 1
+function GoUp() {
+  var steps = 0
+  while (steps < 9) {
+    up();
+    steps = steps + 1
+	}
+}
+
+function GoLeft() {
+  var steps = 0
+  while (steps < 9) {
+    left();
+    steps = steps + 1
+	}
+}
+
+GoDown;
+GoRight;
+GoUp;
+GoLeft;
