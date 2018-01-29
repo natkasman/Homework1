0626653a9c517eb58f0a29d1bed0a8c9d1eccfd3
natkasman <34670412+natkasman@users.noreply.github.com>
2018-01-29 10:51:45 -0800

    Update puzzle4.js

--- puzzle4.js
+++ puzzle4.js
@@ -1,17 +1,49 @@
-down
-down
-down
-down
-right
-var firstColorSeen = getColor
-down
-setColor(firstColorSeen)
-right
-right
-up
-right
-var secondColorSeen = getColor
-down
-setColor(secondColorSeen)
-right
-right
+// Rudy Goes Down
+
+function repeatDown(total) {
+  var count = 0;
+  while (count < total) {
+    down();
+    count = count + 1;
+  }
+}
+
+repeatDown(4);
+
+// Rudy Goes Right
+
+function goRight(total) {
+  var count = 0;
+  while (count < total) {
+    right();
+    count = count + 1;
+  }
+}
+goRight(1);
+
+// Rudy Opens 1st Gate
+
+var firstColorSeen = getColor();
+repeatDown(1);
+setColor (firstColorSeen);
+goRight(2);
+
+// Rudy Goes Up
+
+function goUp(total) {
+  var count = 0;
+  while (count < total) {
+    up();
+    count = count + 1;
+  }
+}
+
+goUp(1);
+goRight(1);
+
+// Rudy Opens 2nd Gate
+
+var secondColorSeen = getColor();
+repeatDown(1);
+setColor(secondColorSeen);
+goRight(2);
