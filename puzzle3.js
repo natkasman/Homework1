b91e2dd6ce325989c2cd63f02f5d7f4baf61f20b
natkasman <34670412+natkasman@users.noreply.github.com>
2018-01-29 10:51:21 -0800

    Update puzzle3.js

--- puzzle3.js
+++ puzzle3.js
@@ -1,12 +1,39 @@
-down
-down
-var firstColorSeen=getColor
-down
-down
-down
-right
-right
-setColor(firstColorSeen)
-right
-right
-up
+// Rudy Goes Down
+
+function repeatDown(total) {
+  var count = 0;
+  while (count < total) {
+    down();
+    count = count + 1;
+  }
+}
+
+repeatDown(2);
+var firstColorSeen = getColor();
+repeatDown(3);
+
+// Rudy Goes Right
+
+function goRight(total) {
+  var count = 0;
+  while (count < total) {
+    right();
+    count = count + 1;
+  }
+}
+goRight(2);
+setColor(firstColorSeen); 
+goRight(2);
+
+
+// Rudy Goes Up
+
+function goUp(total) {
+  var count = 0;
+  while (count < total) {
+    up();
+    count = count + 1;
+  }
+}
+
+goUp(1);
