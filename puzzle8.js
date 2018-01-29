2539ef55706e0b277bc9b8270d37cf7497e071b8
natkasman <34670412+natkasman@users.noreply.github.com>
2018-01-29 10:52:54 -0800

    Update puzzle8.js

--- puzzle8.js
+++ puzzle8.js
@@ -1,31 +1,28 @@
-function GoDown {
-var steps = 0
-while (steps < 8) {
-  down
-  steps = steps + 1
+function GoDown() {
+  var steps = 0
+  while (steps < 8) {
+    down();
+    steps = steps + 1
+	}
+  right();
+  right();
 }
 
-right
-right
 
-function GoUp {
-var steps = 0
-while (steps < 8) {
-  down
-  steps = steps + 1
+function GoUp() {
+  var steps = 0
+  while (steps < 8) {
+    up();
+    steps = steps + 1
+	}
+  right();
+  right();
 }
-}
 
-function GoDown
+GoDown();
+GoUp();
+GoDown();
+GoUp();
+GoDown();
 
-right
-right
-
-function GoUp
-
-right
-right
-
-function GoDown
-
-right
+right();
