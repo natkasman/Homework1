474fd755284fc30b5957b5b1e3dbde766fd2ed02
natkasman <34670412+natkasman@users.noreply.github.com>
2018-01-29 10:50:58 -0800

    Update puzzle2.js

--- puzzle2.js
+++ puzzle2.js
@@ -1,16 +1,35 @@
-down
-down
-down
-down
-down
-down
-right
-right
-right
-up
-up
-right
-right
-down
-down
-down
+// Rudy Goes Down
+
+function repeatDown(total) {
+  var count = 0;
+  while (count < total) {
+    down();
+    count = count + 1;
+  }
+}
+repeatDown(6);
+
+// Rudy Goes Right
+
+function goRight(total) {
+  var count = 0;
+  while (count < total) {
+    right();
+    count = count + 1;
+  }
+}
+goRight(3);
+
+// Rudy Goes Up
+
+function goUp(total) {
+  var count = 0;
+  while (count < total) {
+    up();
+    count = count + 1;
+  }
+}
+
+goUp(2);
+goRight(2);
+repeatDown(3);
